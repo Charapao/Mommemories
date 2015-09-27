5f798a8382c5b18c59ed399d190ecfaba42f8987
masterPao <pao_tia@hotmail.com>
2015-09-27 13:58:14 +0700

    Master_0.3

--- app/src/main/java/com/example/user/mommemories/MainAddNote.java
+++ app/src/main/java/com/example/user/mommemories/MainAddNote.java
@@ -1,43 +1,33 @@
 package com.example.user.mommemories;
 
-import android.support.v7.app.ActionBar;
-import android.content.Intent;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
-
-public class MainNote extends AppCompatActivity {
+public class MainAddNote extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_note);
+        setContentView(R.layout.activity_main_add_note);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
-
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_note, menu);
+        getMenuInflater().inflate(R.menu.menu_main_add_note, menu);
         return true;
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-
         switch (item.getItemId()) {
             case android.R.id.home:
                 // app icon in action bar clicked; goto parent activity.
@@ -46,10 +36,5 @@ public class MainNote extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    public void onClink_tonewnote(View view) {
-        Intent intent = new Intent(this , MainAddNote.class);
-        startActivity(intent);
     }
 }
